4be1449f09f703dad96eaef8349df63e32c2ce00
Yanny Yung <stepiki2000@mail.ru>
2020-04-18 20:40:41 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <cassert>
+#include <unordered_map>
+
+
+class Fibonacci final {
+public:
+	static int get(int n) {
+		assert(n >= 0);
+		if (n == 0 || n == 1) {
+			return n;
+		}
+
+		static std::unordered_map<int, int> cache;
+		auto &result = cache[n];
+
+		if (result == 0) {
+			return get(n - 2) + get(n - 1);
+		}
+		return result;
+	}
+};
+
+int main(int argc, char** argv) {
+	int n;
+	std::cin >> n;
+	std::cout << Fibonacci::get(n) << std::endl;
+	return 0;
+}
