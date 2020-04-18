aec4e06f6f3ec252369d644dfeb297dc595ce2fe
yungstepik <49451602+yungstepik@users.noreply.github.com>
2020-04-18 20:57:20 +0300

    update new 2 versions of calculation

--- ConsoleApplication5/ConsoleApplication5.cpp
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <cassert>
 #include <unordered_map>
+#include <vector>
+#include <ctime>
 
 
 class Fibonacci final {
@@ -22,9 +24,44 @@ public:
 	}
 };
 
+class Fibonacci2 final {
+public:
+	static int get(int n) {
+		assert(n >= 0);
+		static std::unordered_map<int, int> cache;
+		cache[0] = 0;
+		cache[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			
+			cache[i] = cache[i - 2] + cache[i - 1];
+		}
+		return cache[n];
+	}
+};
+
+class Fibonacci3 final {
+public:
+	
+	static int get(int n) {
+		assert(n >= 0);
+		static std::vector<int> cache;
+		cache.resize(n + 1);
+		cache[1] = 1;
+		for (int i = 2; i <= n; i++) {
+
+			cache[i] = cache[i - 2] + cache[i - 1];
+		}
+		return cache[n];
+	}
+};
+
+
 int main(int argc, char** argv) {
+
 	int n;
 	std::cin >> n;
-	std::cout << Fibonacci::get(n) << std::endl;
+	std::cout << Fibonacci3::get(n) << std::endl;
+	unsigned int start = clock();
+	std::cout << start / 1000;
 	return 0;
 }
